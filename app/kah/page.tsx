13447514bf8b173dc971525769fede9848e05edb
Alannah Holder <alannahholder@OMG-CA-OSX-1099.local>
2025-09-23 14:51:19 -0400

    fix: updates Kah 300x250 banner

--- app/kah/page.tsx
+++ app/kah/page.tsx
@@ -7,9 +7,29 @@ import Section from "../Components/Section"
 export default function Home() {
 
   const links = [
+    {
+      text: "160x600",
+      href: "../../kah/FY24_Kahluah_Milk_Spring_Display_Banners_160x600/index.html"
+    },
+     {
+      text: "300x250",
+      href: "../../kah/FY24_Kahluah_Milk_Spring_Display_Banners_300x250/index.html"
+    },
      {
       text: "300x600",
       href: "../../kah/FY24_Kahluah_Milk_Spring_Display_Banners_300x600/index.html"
+    },
+     {
+      text: "320x50",
+      href: "../../kah/FY24_Kahluah_Milk_Spring_Display_Banners_320x50/index.html"
+    },
+     {
+      text: "728x90",
+      href: "../../kah/FY24_Kahluah_Milk_Spring_Display_Banners_728x90/index.html"
+    },
+        {
+      text: "970x250",
+      href: "../../kah/FY24_Kahluah_Milk_Spring_Display_Banners_970x250/index.html"
     },
   ]
 
