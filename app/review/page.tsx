8d83fdc7e343a645bd0a0f375c5059fac00ec9be
Alannah Holder <alannahholder@OMG-CA-OSX-1099.local>
2025-09-26 16:10:18 -0400

    feat: updates all Jef banners

--- app/review/page.tsx
+++ app/review/page.tsx
@@ -6,108 +6,44 @@ import Section from "../Components/Section"
 
 export default function Home() {
 
-  const versatileLinks = [
-    {
-      text: "Versatile - 160x600",
-      href: "../../martell/versatile/F26_Martell_OND_Media_Assets_Versatile_160x600/index.html"
+  const jefLinks = [
+   {
+      text: "160x600",
+      href: "../../jef/FY26_JEF_AlwaysOn_160x600/index.html"
     },
     {
-      text: "Versatile - 300x250",
-      href: "../../martell/versatile/F26_Martell_OND_Media_Assets_Versatile_300x250/index.html"
+      text: "300x250",
+      href: "../../jef/FY26_JEF_AlwaysOn_300x250/index.html"
     },
     {
-      text: "Versatile - 300x600",
-      href: "../../martell/versatile/F26_Martell_OND_Media_Assets_Versatile_300x600/index.html"
+      text: "300x600",
+      href: "../../jef/FY26_JEF_AlwaysOn_300x600/index.html"
     },
     {
-      text: "Versatile - 320x50",
-      href: "../../martell/versatile/F26_Martell_OND_Media_Assets_Versatile_320x50/index.html"
+      text: "320x50",
+      href: "../../jef/FY26_JEF_AlwaysOn_320x50/index.html"
     },
     {
-      text: "Versatile - 728x90",
-      href: "../../martell/versatile/F26_Martell_OND_Media_Assets_Versatile_728x90/index.html"
+      text: "728x90",
+      href: "../../jef/FY26_JEF_AlwaysOn_728x90/index.html"
     },
-      {
-      text: "Versatile - 970x250",
-      href: "../../martell/versatile/F26_Martell_OND_Media_Assets_Versatile_970x250/index.html"
-    },
+    {
+      text: "970x250",
+      href: "../../jef/FY26_JEF_AlwaysOn_970x250/index.html"
+    }
   
   ];
 
-  const giftingLinks = [
-     {
-      text: "Gifting - 160x600",
-      href: "../../martell/gifting/F26_Martell_OND_Media_Assets_Gifting_160x600/index.html"
-    },
-    {
-      text: "Gifting - 300x250",
-      href: "../../martell/gifting/F26_Martell_OND_Media_Assets_Gifting_300x250/index.html"
-    },
-    {
-       text: "Gifting - 300x600",
-      href: "../../martell/gifting/F26_Martell_OND_Media_Assets_Gifting_300x600/index.html"
-    },
-    {
-      text: "Gifting - 320x50",
-      href: "../../martell/gifting/F26_Martell_OND_Media_Assets_Gifting_320x50/index.html"
-    },
-    {
-      text: "Gifting - 728x90",
-      href: "../../martell/gifting/F26_Martell_OND_Media_Assets_Gifting_728x90/index.html"
-    },
-      {
-      text: "Gifting - 970x250",
-      href: "../../martell/gifting/F26_Martell_OND_Media_Assets_Gifting_970x250/index.html"
-    },
-  
-  ]
 
-  const smoothLinks = [
-     {
-      text: "Smoothness - 160x600",
-      href: "../../martell/smoothness/F26_Martell_OND_Media_Assets_Smoothness_160x600/index.html"
-    },
-    {
-      text: "Smoothness - 300x250",
-      href: "../../martell/smoothness/F26_Martell_OND_Media_Assets_Smoothness_300x250/index.html"
-    },
-    {
-      text: "Smoothness - 300x600",
-      href: "../../martell/smoothness/F26_Martell_OND_Media_Assets_Smoothness_300x600/index.html"
-    },
-    {
-      text: "Smoothness - 320x50",
-      href: "../../martell/smoothness/F26_Martell_OND_Media_Assets_Smoothness_320x50/index.html"
-    },
-    {
-      text: "Smoothness - 728x90",
-      href: "../../martell/smoothness/F26_Martell_OND_Media_Assets_Smoothness_728x90/index.html"
-    },
-      {
-      text: "Smoothness - 970x250",
-      href: "../../martell/smoothness/F26_Martell_OND_Media_Assets_Smoothness_970x250/index.html"
-    },
-  
-  ];
 
   return (
     <>
          <Section>
-          <Heading>Martell OND Media Assets</Heading>
-           <h3 className="font-semibold">Gifting</h3>
-           <List>{giftingLinks.map((link) => 
+          <Heading>Jefferson's OND Media Campaign</Heading>
+           <List>{jefLinks.map((link) => 
             <ListItem key={link.href}><Link className="underline hover:no-underline focus:no-underline" href={link.href}>{link.text}</Link></ListItem>
           )}</List>
-
-            <h3 className="font-semibold">Smoothness</h3>
-           <List>{smoothLinks.map((link) => 
-            <ListItem key={link.href}><Link className="underline hover:no-underline focus:no-underline" href={link.href}>{link.text}</Link></ListItem>
-          )}</List>
-
-          <h3 className="font-semibold">Versatile</h3>
-           <List>{versatileLinks.map((link) => 
-            <ListItem key={link.href}><Link className="underline hover:no-underline focus:no-underline" href={link.href}>{link.text}</Link></ListItem>
-          )}</List>
+      
          </Section>
 
          
--- public/jef/FY26_JEF_AlwaysOn_970x250/FY26_JEF_AlwaysOn_970x250.js
+++ public/jef/FY26_JEF_AlwaysOn_970x250/FY26_JEF_AlwaysOn_970x250.js
@@ -36,8 +36,8 @@ tl
     .to("#bg", {duration:0.01, opacity:1, ease:"power0.out"})
 	.fromTo("#border", {opacity: 0 }, { duration: 0, opacity: 1, ease:"power0.out"})
 
-		.fromTo("#headline_a", {y: 25 }, { y: 0, delay:0.25, duration: 0.2, stagger: 1, ease: "slow(0.9, 0.1, false)" })
-	.fromTo("#headline_bourbon", {y: -25 }, { y: 0, duration: 0.2, stagger: 1, ease: "slow(0.9, 0.1, false)" }, "<")
+	.fromTo("#headline_a", {y: 40 }, { y: 0, delay:0.25, duration: 0.2, stagger: 1, ease: "slow(0.9, 0.1, false)" })
+	.fromTo("#headline_bourbon", {y: -40 }, { y: 0, duration: 0.2, stagger: 1, ease: "slow(0.9, 0.1, false)" }, "<")
 
 	.fromTo("#headline", {opacity:0 },{opacity: 1, delay: 0.2, duration: 0.25, stagger: 1, ease: "slow(0.9, 0.1, false)" })
 	.to("#headline", {opacity: 0, delay: 0.2, stagger: 0.75, ease: "power1.out"})
@@ -48,7 +48,6 @@ tl
 	.fromTo("#headline3", { opacity:0 },{ opacity: 1, delay: 0.2, duration: 0.25, stagger: 1, ease: "slow(0.9, 0.1, false)" })
 
 	.to("#firstGroup", {opacity: 0, delay: 0.2, stagger: 0.75, ease: "power1.out"})
-	.fromTo("#bottle", {top: "141px" },{top: "81px",  duration: 0.25, stagger: 1, ease: "slow(0.5, 0.1, false)" })
 
 	.fromTo("#headline4", {x:0, opacity:0 },{x: 0, opacity: 1, delay: 0.05, duration: 0.25, stagger: 1, ease: "power1.out" })
 	.fromTo("#headline5", {x:0, opacity:0 },{x: 0, opacity: 1, delay: 0.05, duration: 0.25, stagger: 1, ease: "power1.out" })
